4d4edda0530f8103a6ab77c1b7905d0df7b21dcc
Yoann Lopes <yoann.lopes@digia.com>
2012-11-08 15:28:16 +0100

    Added default spotify-key.h file.

---
+++ libQtSpotify/spotify_key.h
@@ -0,0 +1,11 @@
+#ifndef SPOTIFY_KEY_H
+#define SPOTIFY_KEY_H
+
+#error
+#error "You need to set g_appkey with a libspotify API key provided by Spotify. Please see https://developer.spotify.com/en/libspotify/application-key/"
+#error
+
+const uint8_t g_appkey[] = { };
+const size_t g_appkey_size = sizeof(g_appkey);
+
+#endif // SPOTIFY_KEY_H
